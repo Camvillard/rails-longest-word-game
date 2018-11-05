49369eca301f1984e883640dbd1345deae959b84
camille villard <hello@camillevillard.com>
2018-11-05 15:21:02 -0500

    implemented the score method

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,40 @@
+require 'open-uri'
+# Games controller
 class GamesController < ApplicationController
   def new
     @letters = []
-    letter = 10.times do |l|
-      l = ("a".."z").to_a.sample
-      @letters << l
+    10.times do
+      @letters << ('a'..'z').to_a.sample
     end
+    # render new.html.erb
+  end
+
+  def check_is_the_word_exists(attempt)
+    url = "https://wagon-dictionary.herokuapp.com/#{attempt.downcase}"
+    serialized = open(url).read
+    data = JSON.parse(serialized)
+    data['found']
+  end
+
+  def valid_grid?(attempt, grid)
+    splitted_word = attempt.downcase.split('')
+    valid_grid = true
+    splitted_word.each do |letter|
+      valid_grid = false unless grid.include?(letter)
+      grid.delete_at(grid.index(letter)) if grid.include?(letter)
+    end
+    valid_grid
   end
 
   def score
+    grid = params['grid'].split
+    user_word = params['user_input']
+    if !check_is_the_word_exists(user_word)
+      @feedback = 'this word does not exists'
+    elsif !valid_grid?(user_word, grid)
+      @feedback = "this word doesn't match, dude"
+    else
+      @feedback = 'cool'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'games/new'
-  get 'games/score'
+  get 'games/new', to: 'games#new', as: :new
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'games/score', to: 'games#score', as: :score
 end
